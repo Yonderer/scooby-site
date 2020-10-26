c26c91ef96917ecb4b0a519d4063afb957f4eba9
Yonderer <63592517+Yonderer@users.noreply.github.com>
2020-10-26 17:15:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,15 @@ def home():
 def about():
     return render_template('pages/placeholder.about.html')
 
+@app.errorhandler(500)
+def internal_error(error):
+    #db_session.rollback()
+    return render_template('errors/500.html'), 500
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('errors/404.html'), 404
 
 if __name__ == '__main__':
     app.run()
